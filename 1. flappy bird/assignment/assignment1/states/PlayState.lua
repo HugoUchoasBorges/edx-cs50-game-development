a687866a741ad9d6bb06b2ebfe4b93ee6b4520c8
Hugo Uchôas Borges <hugoborges@unifei.edu.br>
2019-07-01 19:07:07 -0300

    [ADD] Spawn Interval Variant

--- 1. flappy bird/assignment/assignment1/states/PlayState.lua
+++ 1. flappy bird/assignment/assignment1/states/PlayState.lua
@@ -17,6 +17,9 @@ PIPE_HEIGHT = 288
 BIRD_WIDTH = 38
 BIRD_HEIGHT = 24
 
+local SPAWN_INTERVAL = 2
+local SPAWN_INTERVAL_VARIANT = 1
+
 function PlayState:init()
     self.bird = Bird()
     self.pipePairs = {}
@@ -32,7 +35,7 @@ function PlayState:update(dt)
     self.timer = self.timer + dt
 
     -- spawn a new pipe pair every second and a half
-    if self.timer > 2 then
+    if self.timer > (SPAWN_INTERVAL + SPAWN_INTERVAL_VARIANT) then
         -- modify the last Y coordinate we placed so pipe gaps aren't too far apart
         -- no higher than 10 pixels below the top edge of the screen,
         -- and no lower than a gap length (90 pixels) from the bottom
@@ -45,6 +48,7 @@ function PlayState:update(dt)
 
         -- reset timer
         self.timer = 0
+        SPAWN_INTERVAL_VARIANT = 2*(math.random()-0.2)
     end
 
     -- for every pair of pipes..
