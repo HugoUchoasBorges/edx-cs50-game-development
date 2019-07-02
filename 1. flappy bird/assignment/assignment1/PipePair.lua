8cf02d3def5c33d105323b4b21ea752a09a02201
Hugo Uchôas Borges <hugoborges@unifei.edu.br>
2019-07-02 00:54:51 -0300

    [IMP] PipePair GAP

--- 1. flappy bird/assignment/assignment1/PipePair.lua
+++ 1. flappy bird/assignment/assignment1/PipePair.lua
@@ -27,7 +27,7 @@ function PipePair:init(y)
     -- instantiate two pipes that belong to this pair
     self.pipes = {
         ['upper'] = Pipe('top', self.y),
-        ['lower'] = Pipe('bottom', self.y + PIPE_HEIGHT + GAP_HEIGHT - math.random(-GAP_HEIGHT_RANDOM, GAP_HEIGHT_RANDOM))
+        ['lower'] = Pipe('bottom', self.y + PIPE_HEIGHT + GAP_HEIGHT - math.random(-2*GAP_HEIGHT_RANDOM, GAP_HEIGHT_RANDOM))
     }
 
     -- whether this pipe pair is ready to be removed from the scene
