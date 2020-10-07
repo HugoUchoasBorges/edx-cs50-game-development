8c48cfc9a1cbff9d72122ef2d794bfeb3f70f63e
Hugo Borges <hugouchoas@outook.com>
2020-10-06 22:02:08 -0300

    GameOver scene working

---
+++ 9. Dreadhalls/assignment9/Assets/Scripts/DespawnOnHeight.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DespawnOnHeight : MonoBehaviour
+{
+    [SerializeField] [Range(-5, 0f)] private int minHeight = -1;
+
+    private bool despawned = false;
+
+    private void Start()
+    {
+        despawned = false;
+    }
+
+    void Update()
+    {
+        if (!despawned && transform.position.y < minHeight)
+        {
+            despawned = true;
+            GameOver();
+        }
+    }
+
+    private void GameOver()
+    {
+        Debug.Log("GAME OVER");
+        Destroy(GameObject.Find("WhisperSource"));
+        SceneManager.LoadScene("GameOver");
+    }
+}
