d34e022b615d7c77c075f9775e142528004c843c
Hugo Borges <hugouchoas@outook.com>
2020-10-10 09:16:19 -0300

    Level Complete

---
+++ 10. Portal/assignment10/Assets/Scripts/Assignment/MovablePlatform.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace assignment
+{
+    public class MovablePlatform : MonoBehaviour
+    {
+        [SerializeField] private Transform[] positions;
+        [SerializeField] [Range(1f, 10f)] private float speed = 1f;
+        private int currPosition = 0;
+
+        private void Start()
+        {
+            StartCoroutine(move());
+        }
+
+        private IEnumerator move()
+        {
+            Vector3 from = positions[currPosition].position;
+            int nextPosition = currPosition + 1 >= positions.Length ? 0 : currPosition + 1;
+            Vector3 to = positions[nextPosition].position;
+
+            while (true)
+            {
+                from = positions[currPosition].position;
+                nextPosition = currPosition + 1 >= positions.Length ? 0 : currPosition + 1;
+                to = positions[nextPosition].position;
+
+                float step = (speed / (from - to).magnitude) * Time.fixedDeltaTime;
+                float t = 0;
+                while (t <= 1.0f)
+                {
+                    t += step; // Goes from 0 to 1, incrementing by step each time
+                    transform.position = Vector3.Lerp(from, to, t); // Move objectToMove closer to b
+                    yield return new WaitForFixedUpdate();         // Leave the routine and return here in the next frame
+                }
+                transform.position = to;
+                currPosition = nextPosition;
+
+            }
+        }
+    }
+}
---
+++ 10. Portal/assignment10/Assets/Scripts/Assignment/TextTrigger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace assignment
+{
+    public class TextTrigger : MonoBehaviour
+    {
+        [SerializeField] private Text text;
+        [SerializeField] [Range(0f, 10f)] private float screenTime = 3;
+
+        private const string PLAYER_TAG = "Player";
+
+        private void Awake()
+        {
+            if (text.isActiveAndEnabled)
+                text.enabled = false;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == PLAYER_TAG)
+            {
+                text.enabled = true;
+                StartCoroutine(Util.HideTextDelay(text, screenTime));
+            }
+        }
+    }
+}
