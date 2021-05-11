6287cd3b0dfa02237a796981d4e8b4afb0b1557f
Hugo Borges <hugouchoas@outook.com>
2021-05-10 23:00:17 -0300

    [PUC-4731] Added default background

---
+++ 11. Final/edx_final/Assets/Assets/Scripts/Background/BackgroundController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace background
+{
+    public class BackgroundController : MonoBehaviour
+    {
+        [Header("Components")]
+        [SerializeField] private ParticleSystem _bgParticles;
+
+        private void Start()
+        {
+            InitBackground();
+        }
+
+
+        // ========================== Init ============================
+
+        private void InitBackground()
+        {
+            _bgParticles.Play();
+        }
+    }
+}
